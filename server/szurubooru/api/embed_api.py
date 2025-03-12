f9794260b334d91203bfb362ddc9a18c54d2c558
hujle <16465620+hujle@users.noreply.github.com>
2025-03-12 14:59:15 +0300

    second attempt failed

--- server/szurubooru/api/embed_api.py
+++ server/szurubooru/api/embed_api.py
@@ -63,8 +63,6 @@ def get_post(
     post_id = _get_post_id(match)
     post = _get_post(post_id)
     serialized = _serialize_post(ctx, post)
-    if serialized.safety == model.Post.SAFETY_HIDDEN:
-        auth.verify_privilege(ctx.user, "posts:view:hidden")
     embed = {
         "version": "1.0",
         "type": "photo",
